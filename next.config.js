ca606318376e0e7be7f11d2b2629a7692bc83646
Haider Zaman <chhaider10954@gmail.com>
2025-07-30 19:23:01 +0500

    Changes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const nextConfig = {
   output: "export", // ✅ enables static export
+  basePath: "/tt", // 👈 VERY IMPORTANT
+  assetPrefix: "/tt", // 👈 ensure CSS/JS loads correctly
 };
 
 module.exports = nextConfig;
