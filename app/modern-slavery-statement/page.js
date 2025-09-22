60783c4b27cb7212f24f119b57395d8efce15152
Haider Zaman <chhaider10954@gmail.com>
2025-09-22 18:11:41 +0500

    Update page.js

--- app/modern-slavery-statement/page.js
+++ app/modern-slavery-statement/page.js
@@ -31,7 +31,7 @@ export default function ModernSalaveryStatementPage() {
         <div className="max-w-6xl mx-auto"
           style={{ marginTop: shouldOffset ? `${headerHeight}px` : undefined }}
         >
-          <h4>HYPERAMA GROUP MODERN SLAVERY STATEMENT</h4>
+          <h4 className="eczar mb-3">HYPERAMA GROUP MODERN SLAVERY STATEMENT</h4>
           <p className="mb-3">
             Thank you for reading this post, don't forget to subscribe! Tiger
             Tiger is a brand of Hyperama Plc. Our statement sets out the steps
@@ -39,7 +39,7 @@ export default function ModernSalaveryStatementPage() {
             responsibilities under the Modern Slavery Act 2015.
           </p>
 
-          <h4>INTRODUCTION FROM THE BOARD OF DIRECTORS</h4>
+          <h4 className="eczar mb-3">INTRODUCTION FROM THE BOARD OF DIRECTORS</h4>
           <p className="mb-3">
             Modern slavery includes human trafficking, slavery or forced labour
             which can exist in all economies and businesses. We as a Company are
@@ -53,7 +53,7 @@ export default function ModernSalaveryStatementPage() {
             within the organisation and through our supply chain.
           </p>
 
-          <h4>OUR COMPANY</h4>
+          <h4 className="eczar mb-3">OUR COMPANY</h4>
           <p className="mb-3">
             Hyperama plc, which includes Tiger Tiger, is a privately-owned
             family Business and are one of the leading importers and suppliers
@@ -68,7 +68,7 @@ export default function ModernSalaveryStatementPage() {
             continues to expand and thrive in a variety of competitive markets.
           </p>
 
-          <h4>SUPPLY CHAIN</h4>
+          <h4 className="eczar mb-3">SUPPLY CHAIN</h4>
           <p className="mb-3">
             We are committed to building long standing relationships with our
             suppliers and make clear our expectations and business conduct. We
@@ -77,38 +77,38 @@ export default function ModernSalaveryStatementPage() {
             of disclosure within their own organisation.
           </p>
 
-          <h4>STANDARDS IN OUR BUSINESS</h4>
+          <h4 className="eczar mb-3">STANDARDS IN OUR BUSINESS</h4>
           <p className="mb-3">
             The standards which we set in our business consist of the following:
           </p>
-          <p>
+          <p className="mb-3">
             Comply with all local laws in regards to working hours and time
             periods
           </p>
-          <p>
+          <p className="mb-3">
             Ensure that workers pay complies with local laws, for instance
             minimum wage standards
           </p>
-          <p>
+          <p className="mb-3">
             Ensure all workers are treated with respect and dignity and exclude
             all forms of harassment, intimidation, abuse and violence No forced,
             involuntary labour, slavery or human trafficking
           </p>
-          <p>
+          <p className="mb-3">
             No discrimination against workforce on any basis or on personal
             characteristics (racial, colour, race and religion)
           </p>
-          <p>
+          <p className="mb-3">
             Creating a fair and equal workplace by providing equal opportunities
             and diverse workforce
           </p>
-          <p>
+          <p className="mb-3">
             Ensure a safe and hygienic place of work for employees and operate a
             safe system of work to comply with all local health and safety
             legislations
           </p>
 
-          <h4>EMPLOYMENT</h4>
+          <h4 className="eczar mb-3">EMPLOYMENT</h4>
           <p className="mb-3">
             We hold a robust recruitment procedure in line with UK legislation,
             including right to work document checks, contracts of employment and
@@ -116,26 +116,26 @@ export default function ModernSalaveryStatementPage() {
             securely and confidentially
           </p>
 
-          <h4>POLICIES/PROCEDURES</h4>
-          <p>
+          <h4 className="eczar mb-3">POLICIES/PROCEDURES</h4>
+          <p className="mb-3">
             We have various policies and procedures in place to commit tackling
             slavery and human trafficking such as:
           </p>
-          <p>
+          <p className="mb-3">
             Equal opportunities policy- committed to ensuring that our
             workplaces are free from unlawful or unfair discrimination because
             of protected characteristics as defined by the Equality Act 2010
           </p>
 
-          <p>
+          <p className="mb-3">
             Bullying & Harassment Policy- Committed to ensuring that our
             employees have a working environment which respects their personal
             dignity Equal Pay Policy- committed to the principle of equal pay
             for men and women
           </p>
 
-          <h4>ENDORSEMENT OF THIS STATEMENT</h4>
-          <p>
+          <h4 className="eczar mb-3">ENDORSEMENT OF THIS STATEMENT</h4>
+          <p className="mb-3">
             This statement is approved on behalf of the Board of Directors of
             Hyperama Plc on 16th August 2023
           </p>
